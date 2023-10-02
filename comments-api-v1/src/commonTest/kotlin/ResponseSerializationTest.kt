9147f37262d57f57862c7ca9ce33a32e75421e73
StellaLupus <soulofpain.k@gmail.com>
2023-10-02 19:03:38 +0300

    add: api-v1 fix: документация api

---
+++ comments-api-v1/src/commonTest/kotlin/ResponseSerializationTest.kt
@@ -0,0 +1,46 @@
+import com.crowdproj.product.comments.api.v1.models.*
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class ResponseSerializationTest{
+    private val response: ICommentResponse = CommentCreateResponse(
+        requestId = "241",
+        result = ResponseResult.SUCCESS,
+        comment = CommentResponseObject(
+            id = "5123",
+            objectType = "ad",
+            objectId = "211124",
+            userId = "12111",
+            content = "This comment of the ad",
+            contentType = "plainText",
+            createdAt = "2023-01-11T12:22:53Z",
+            updatedAt = "2023-01-11T12:22:53Z"
+        )
+    )
+
+    @Test
+    fun serialize() {
+        val json = Json.encodeToString(ICommentResponse.serializer(), response)
+
+        assertContains(json, Regex("\"requestId\":\\s*\"241\""))
+        assertContains(json, Regex("\"id\":\\s*\"5123\""))
+        assertContains(json, Regex("\"objectType\":\\s*\"ad\""))
+        assertContains(json, Regex("\"objectId\":\\s*\"211124\""))
+        assertContains(json, Regex("\"userId\":\\s*\"12111\""))
+        assertContains(json, Regex("\"content\":\\s*\"This comment of the ad\""))
+        assertContains(json, Regex("\"contentType\":\\s*\"plainText\""))
+        assertContains(json, Regex("\"createdAt\":\\s*\"2023-01-11T12:22:53Z\""))
+        assertContains(json, Regex("\"updatedAt\":\\s*\"2023-01-11T12:22:53Z\""))
+
+    }
+
+    @Test
+    fun deserialize() {
+        val json = Json.encodeToString(ICommentResponse.serializer(), response)
+        val obj = Json.decodeFromString(ICommentResponse.serializer(), json) as CommentCreateResponse
+
+        assertEquals(response, obj)
+    }
+}
